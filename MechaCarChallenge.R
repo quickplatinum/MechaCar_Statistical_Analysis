52121740d0c1d7dcd04996e1548b8143af7a0b07
quickplatinum <88692025+quickplatinum@users.noreply.github.com>
2021-12-12 23:47:24 -0500

    Module 15 Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,7 +14,7 @@ summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,
 
 # Deliverable 2: Create Visualizations for the Trip Analysis
 
-# Create Supension Coil Lot Analysis
+# Create Suspension Coil Lot Analysis
 # Create a Data Frame from CSV file
 suspension_coil_df <- read.csv(file = './resources/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
 head(suspension_coil_df)
@@ -22,3 +22,14 @@ head(suspension_coil_df)
 total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 # Create specific summaries for each lot
 lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+
+# Deliverable 3: T-Tests on Suspension Coils
+
+
+# Create T-Tests on suspension coils
+t.test(suspension_coil_df$PSI, mu=1500)
+
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
